58dec20d05233b579ff5c16fb5b4dada9f452769
RenvezWilliam <renvezwilliam@gmail.com>
2025-02-20 16:47:03 +0100

    WR 20/02 : Ajout de la possibilité de faires plusieurs figures

--- creator.py
+++ creator.py
@@ -2,85 +2,119 @@ import pygame
 from pygame.locals import *
 import sys
 
-import gmsh_creator as gc
+import figure_creator as fc
+
+## Initialisation des couleurs ##
+WHITE           = (255, 255, 255)
+ANCHOR_BIG      = (100, 100, 100)
+ANCHOR_NORMAL   = (150, 150, 150)
+ANCHOR_SMALL    = (200, 200, 200)
+
+CURRENT_POINT   = (100, 100, 255)
+LAST_POINT      = (255, 200, 50)
+CURRENT_LINE    = (200, 50, 255)
+LAST_LINE       = (255, 50, 50)
+
+OTHER_POINT     = (204, 152, 255)
+OTHER_LINE      = (204, 152, 255)
 
 class Game:
 
     def __init__(self):
         pygame.init()
-        self.size                   = self.width, self.height = 500, 500
-        self.window                 = pygame.display.set_mode(self.size)
-        self.clock                  = pygame.time.Clock().tick(30)
+        self.size                       = self.width, self.height = 500, 500
+        self.window                     = pygame.display.set_mode(self.size)
+        self.clock                      = pygame.time.Clock().tick(30)
+        self.title                      = pygame.display.set_caption('Editeur de figure')
 
-        ## Initialisation des couleurs ##
-        self.WHITE                  = (255, 255, 255)
-        self.ANCHOR_BIG             = (100, 100, 100)
-        self.ANCHOR_NORMAL          = (150, 150, 150)
-        self.ANCHOR_SMALL           = (200, 200, 200)
-        self.POINT                  = (100, 100, 255)
-        self.LAST_POINT             = (255, 200, 50)
-        self.LINE                   = (200, 50, 255)
-        self.LAST_LINE              = (255, 50, 50)
+        self.figures                    = []
+        self.figures.append(Figure())
+        self.current_selected_figure    = 0
 
         ## Initialisation des variables ##
-        self._ctrl, self._shift     = False, False
-        self.points                 = []
+        self._ctrl, self._shift         = False, False
 
     def refresh(self):
-        self.window.fill(self.WHITE)
+        self.window.fill(WHITE)
 
         if self._ctrl and not self._shift   : self.draw_large_anchor_lines()
         if not self._ctrl and self._shift   : self.draw_anchor_lines()
         if self._ctrl and self._shift       : self.draw_small_anchor_lines()
 
-        self.draw_lines()
-        self.draw_points()
+        self.draw()
     
     def draw_large_anchor_lines(self):
         for i in range(0, 500, 100):
-            pygame.draw.line(self.window, self.ANCHOR_BIG, (0, i), (500, i))
-            pygame.draw.line(self.window, self.ANCHOR_BIG, (i, 0), (i, 500))
+            pygame.draw.line(self.window, ANCHOR_BIG, (0, i), (500, i))
+            pygame.draw.line(self.window, ANCHOR_BIG, (i, 0), (i, 500))
 
     def draw_anchor_lines(self):
         for i in range(0, 500, 50):
-            pygame.draw.line(self.window, self.ANCHOR_NORMAL, (0, i), (500, i))
-            pygame.draw.line(self.window, self.ANCHOR_NORMAL, (i, 0), (i, 500))
+            pygame.draw.line(self.window, ANCHOR_NORMAL, (0, i), (500, i))
+            pygame.draw.line(self.window, ANCHOR_NORMAL, (i, 0), (i, 500))
             
             self.draw_large_anchor_lines()
 
     def draw_small_anchor_lines(self):
         for i in range(0, 500, 10):
-            pygame.draw.line(self.window, self.ANCHOR_SMALL, (0, i), (500, i))
-            pygame.draw.line(self.window, self.ANCHOR_SMALL, (i, 0), (i, 500))
+            pygame.draw.line(self.window, ANCHOR_SMALL, (0, i), (500, i))
+            pygame.draw.line(self.window, ANCHOR_SMALL, (i, 0), (i, 500))
 
             self.draw_anchor_lines()
 
+    def add_point(self, x, y):
+        is_present = False
+        for points in self.figures[self.current_selected_figure].points:
+            if points[0] == x and points[1] == y:
+                is_present = True
+        
+        if not is_present:
+            self.figures[self.current_selected_figure].add_point(x, y)
 
 
-    def draw_points(self):
-        if (len(self.points) > 0):
-            for i in range(len(self.points) - 1):
-                pygame.draw.circle(self.window, self.POINT, self.points[i], 5)
-            pygame.draw.circle(self.window, self.LAST_POINT, self.points[-1], 5)
+    def draw(self): # Pour toutes les figures, lance les dessins
+        for i in range(len(self.figures)):
+            self.figures[i].draw_points(self.window, False) if self.current_selected_figure != i else self.figures[i].draw_points(self.window, True)
+            self.figures[i].draw_lines(self.window, False) if self.current_selected_figure != i else self.figures[i].draw_lines(self.window, True)
 
-    def draw_lines(self):
-        if (len(self.points) > 1):
-            for i in range(len(self.points) - 1):
-                pygame.draw.line(self.window, self.LINE, self.points[i], self.points[i + 1], 2)
-            pygame.draw.line(self.window, self.LAST_LINE, self.points[-1], self.points[0], 2)
+    def remove_last_point(self): ## IF NOT remove_last_point() pour la figure en cours -> regarde si on possède plus d'une figure. si oui, supprime la figure.
+        if self.figures[self.current_selected_figure].remove_last_point():
+            pass
+        else:
+            if len(self.figures) > 1:
+                self.remove_current_figure()
+                print('figure supprimée')
+        
+    def remove_current_figure(self):
+        self.figures.pop(self.current_selected_figure)
+        self.current_selected_figure = self.current_selected_figure % len(self.figures)
 
-    def remove_last_point(self):
-        if len(self.points) > 0:
-            self.points.pop(-1)
+    def save(self, mode = 'tri'): ## A REFAIRE INTEGRALEMENT
+        
+        points = []
 
-    def save(self, mode = 'tri'):
-        if len(self.points) < 3:
-            print("Vous avez besoin d'au moins 3 points pour sauvegarder.")
-            return
-        gc.automatize(self.points, mode)
+        for f in self.figures:
+            points.append(f.points)
+            if len(f.points) < 3:
+                print('Une des surfaces possède moins de 3 points, il ne peut pas être sauvegardée')
+                return
+        
+
+        fc.automatize(points=points, mode=mode)
+
+    def add_new_figure(self):
+        self.figures.append(Figure())
+        self.current_selected_figure = len(self.figures) - 1
+    
+    def next_figure(self):
+        self.current_selected_figure = (self.current_selected_figure + 1) % (len(self.figures))
+
+    def previous_figure(self):
+        self.current_selected_figure = (self.current_selected_figure - 1) % (len(self.figures))
+
+        
 
     def events(self):
-
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LSHIFT]: _shift = True
@@ -91,11 +125,14 @@ class Game:
                 sys.exit()
 
             if event.type == pygame.KEYDOWN:
-                if pygame.key.name(event.key) == 'left shift': self._shift = True
-                if pygame.key.name(event.key) == 'left ctrl' : self._ctrl  = True
-                if pygame.key.name(event.key) == 'r'         : self.remove_last_point()
-                if pygame.key.name(event.key) == 't'         : self.save("tri")
-                if pygame.key.name(event.key) == 'y'         : self.save("quad")
+                if pygame.key.name(event.key) == 'left shift'   : self._shift = True
+                if pygame.key.name(event.key) == 'left ctrl'    : self._ctrl  = True
+                if pygame.key.name(event.key) == 'r'            : self.remove_last_point()
+                if pygame.key.name(event.key) == 't'            : self.save("tri")
+                if pygame.key.name(event.key) == 'y'            : self.save("quad")
+                if pygame.key.name(event.key) == 'n'            : self.add_new_figure()
+                if pygame.key.name(event.key) == 'up'           : self.next_figure()
+                if pygame.key.name(event.key) == 'down'         : self.previous_figure()
             
             if event.type == pygame.KEYUP:
                 if pygame.key.name(event.key) == 'left shift': self._shift = False
@@ -116,12 +153,39 @@ class Game:
                     x = round(x / 10) * 10
                     y = round(y / 10) * 10
 
-                is_present = False
-                for p in self.points:
-                    if p == (x, y): is_present = True
-                        
-                if not is_present : self.points.append((x, y))
+                self.add_point(x, y)
 
+class Figure:
+    def __init__(self):
+        self.points = []
+    
+    def add_point(self, x, y):
+        self.points.append((x, y))
+    
+    def remove_last_point(self) -> bool: # Si return False et que l'on souhaite retirer un point, on pourrait simplement détruire la figure avec la reception du False
+        if len(self.points) == 0:
+            return False
+
+        self.points.pop(-1)
+        return True
+    
+    def draw_points(self, window, current_figure: bool):
+
+        if len(self.points) <= 0: # Il ne possède pas de points, donc pas de dessin, puni !
+            return
+        
+        for i in range(len(self.points) - 1):
+            pygame.draw.circle(window, CURRENT_POINT, self.points[i], 5) if current_figure else pygame.draw.circle(window, OTHER_POINT, self.points[i], 3)
+        pygame.draw.circle(window, LAST_POINT, self.points[-1], 5) if current_figure else pygame.draw.circle(window, OTHER_POINT, self.points[-1], 3)
+
+    def draw_lines(self, window, current_figure: bool):
+
+        if len(self.points) <= 1: # Il ne possède pas assez de points, pas de ligne, encore puni !
+            return
+        
+        for i in range(len(self.points) - 1):
+            pygame.draw.line(window, CURRENT_LINE, self.points[i], self.points[i + 1], 2) if current_figure else pygame.draw.line(window, OTHER_LINE, self.points[i], self.points[i + 1], 2)
+        pygame.draw.line(window, LAST_LINE, self.points[-1], self.points[0], 2) if current_figure else pygame.draw.line(window, OTHER_LINE, self.points[-1], self.points[0], 2)
             
 if __name__ == '__main__':
 
@@ -136,6 +200,9 @@ if __name__ == '__main__':
     # print("• Bouton 'S'             -> Sauvegarder en un fichier MSH")
     print("• Bouton 'T'             -> Sauvegarder un fichier MSH avec un maillage triangulaire")
     print("• Bouton 'Y'             -> Sauvegarder un fichier MSH avec un maillage quadrangulaire")
+    print("• Bouton 'N'             -> Nouvelle figure")
+    print("• Bouton 'up'            -> Figure suivante")
+    print("• Bouton 'down'          -> Figure précédente")
 
     while True:
         game.refresh()
---
+++ figure_creator.py
@@ -0,0 +1,111 @@
+import gmsh
+import math # Pour la création de l'étoile dans le if __name__
+
+from datetime import datetime
+from typing import Literal
+
+class FIGURE_CREATOR:
+    def __init__(self):
+        self.points  = []
+        self.surface = None
+        self.fig_id  = 0
+
+        self.points.append([])
+    
+    def add_fig(self):
+        self.points.append([])
+        self.fig_id += 1
+
+    def initialize(self):
+        gmsh.initialize()
+        gmsh.clear()
+        gmsh.model.add("form")
+
+    def add_point(self, x, y):
+        self.points[self.fig_id].append(gmsh.model.geo.add_point(x, y, 0, 1))
+
+    def create_surface(self):
+        # Créer les lignes pour toutes les surfaces.
+
+        
+        lines = []
+        for i in range(len(self.points)):
+            lines.append([])
+            for j in range(len(self.points[i]) - 1):        
+                lines[i].append(gmsh.model.geo.addLine(self.points[i][j], self.points[i][j + 1]))
+            lines[i].append(gmsh.model.geo.addLine(self.points[i][-1], self.points[i][0]))
+            
+            
+        # Créer les loops pour chaque surfaces
+        loops = []
+        print(self.points)
+        print(lines)
+        for i in range(len(lines)):
+            loops.append(gmsh.model.geo.add_curve_loop(lines[i]))
+
+        # Créer la surface à partir des loops
+        surface = gmsh.model.geo.add_plane_surface(loops)
+
+    def finilize(self, mode : Literal['tri', 'quad']):
+        if mode == 'quad':
+            gmsh.option.set_number("Mesh.RecombineAll", 1)
+
+        ## Créer le maillage
+        gmsh.model.geo.synchronize()
+        gmsh.model.mesh.generate(1)
+        gmsh.model.mesh.generate(2)
+
+    def view_mesh(self):
+        gmsh.fltk.run()
+
+    def save(self, filename):
+        gmsh.write(filename+'.msh')
+
+##########################################################################
+
+def automatize(points, mode: Literal['tri', 'quad'] = "tri"):
+    fc = FIGURE_CREATOR()
+    fc.initialize()
+
+
+    for i, pp in enumerate(points):
+        
+        for p in pp:
+            p = (float(p[0] / 100), float(500 - p[1]/100))
+            fc.add_point(p[0], p[1])
+        
+        if i != len(points) - 1:
+            fc.add_fig() 
+
+    fc.create_surface()
+    fc.finilize(mode)
+
+    if mode == 'tri':
+        fc.save(f'mesh_tri_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}')
+    else:
+        fc.save(f'mesh_quad_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}')
+
+    # fc.view_mesh()
+
+##########################################################################
+
+if __name__ == "__main__":
+    """ Création d'une étoile """ # Obsolète
+
+    # Initialisation des Variables :
+    points = []
+    center_x, center_y = 2.5, 2.5
+    outer_radius = 2
+    inner_radius = 0.9
+    angle_offset = math.radians(90)
+
+    # Mise en place des points de l'étoile
+    for i in range(10):
+        radius = outer_radius if i % 2 == 0 else inner_radius
+        angle = angle_offset + i * math.pi / 5
+        x = center_x + radius * math.cos(angle)
+        y = center_y + radius * math.sin(angle)
+        points.append((x, y))
+    
+    automatize(points, 'tri') # Récupère un maillage triangulaire de l'étoile
+    automatize(points, 'quad') # Récupère un mallage quandrangulaire de l'étoile
